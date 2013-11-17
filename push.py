8a2873a2f0ef91f3438d9c76727effd66355dc10
thekaratekid552 <thekaratekid552@gmail.com>
2013-11-17 13:17:57 -0500

    Added ability to change branch.

--- push.py
+++ push.py
@@ -1,5 +1,6 @@
 import os
+branch = str(raw_input("What branch to push to? "))
 msg = str(raw_input("Please enter a commit reason: "))
 os.system("git add -A")
 os.system("git commit -m \"{0}\"".format(msg))
-os.system("git push origin master")
+os.system("git push origin {0}".format(branch))
